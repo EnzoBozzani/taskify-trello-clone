99025e532a6f3cc6c1b0c30106a5c5525ce1b72b
Enzo Bozzani <bozzanienzo@gmail.com>
2024-01-22 03:45:45 -0300

    feat: started adding server actions

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,24 @@
-const OrganizationIdPage = () => {
-	return <div>Organization Page</div>;
+import { create } from '@/actions/createBoard';
+import { Button } from '@/components/ui/button';
+import { db } from '@/lib/db';
+
+const OrganizationIdPage = async () => {
+	const boards = await db.board.findMany();
+
+	return (
+		<div>
+			<form action={create}>
+				<input
+					id='title'
+					name='title'
+					required
+					placeholder='Enter board title'
+					className='border-black border p-1'
+				/>
+				<Button type='submit'>Submit</Button>
+			</form>
+		</div>
+	);
 };
 
 export default OrganizationIdPage;
